e47d6684125599b6bd2ef0ebf3c775c499898e37
Aaron Junod <tranqy@snowball.local>
2019-09-22 20:21:20 -0400

    Added link actions

--- lib/components/contact_icons.dart
+++ lib/components/contact_icons.dart
@@ -1,6 +1,7 @@
 import 'package:aaron_junod_dev/models/resume_content.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactIcons extends StatelessWidget {
   final ResumeContent resumeContent;
@@ -19,24 +20,26 @@ class ContactIcons extends StatelessWidget {
             color: Colors.white54,
             icon: new Icon(FontAwesomeIcons.linkedin),
             onPressed: () {
-              print("Pressed");
+              _launchURL("http://linkedin.com/in/${resumeContent.linkedIn}");
             }),
         IconButton(
             // Use the FontAwesomeIcons class for the IconData
             color: Colors.white54,
             icon: new Icon(FontAwesomeIcons.github),
             onPressed: () {
-              print("Pressed");
+              _launchURL("http://github.com/${resumeContent.github}");
             }),
         IconButton(
             // Use the FontAwesomeIcons class for the IconData
             color: Colors.white54,
             icon: new Icon(FontAwesomeIcons.twitter),
             onPressed: () {
-              print("Pressed");
+              _launchURL("http://twitter.com/${resumeContent.twitter}");
             }),
         FlatButton(
-          onPressed: () {},
+          onPressed: () {
+              _launchURL("mailto:${resumeContent.email}");
+          },
           child: Text(resumeContent.email,
               style: TextStyle(
                   color: Colors.white54,
@@ -46,4 +49,15 @@ class ContactIcons extends StatelessWidget {
       ],
     );
   }
+  _launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+
+
+
 }
